000d0e9d2de5ba17331fdb05c349cbef84091a40
Siddhesh Murlidhar Yadav <92988145+Siddhesh-Yadav@users.noreply.github.com>
2022-04-11 16:39:46 +0530

    some

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,7 +16,7 @@ const Navbar = () => {
   }, [location])
   
   return (
-    <nav className="navbar navbar-dark bg-dark">
+    <nav className="navbar navbar-expand-xs navbar-dark bg-dark">
     <div className="container-fluid">
       <Link className="navbar-brand" to="/">Navbar</Link>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
